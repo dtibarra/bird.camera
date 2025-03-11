a5c0e2ed4446068d15e2d0a6aaac679f688094b7
David Ibarra <dtibarra@gmail.com>
2025-03-11 00:26:25 -0500

    add 2nd video

--- src/js/bird.js
+++ src/js/bird.js
@@ -66,7 +66,32 @@ if (video) {
         video.src = videoSrc;
     }
 }
-
+var video2 = document.getElementById('video2');
+if (video2) {
+    var videoSrc2 = 'https://bird.camera/stream2/index.m3u8';
+    if (Hls.isSupported()) {
+        var hls = new Hls();
+        hls.loadSource(videoSrc2);
+        hls.attachMedia(video2);
+        video2.play();
+    }
+    // HLS.js is not supported on platforms that do not have Media Source
+    // Extensions (MSE) enabled.
+    //
+    // When the browser has built-in HLS support (check using `canPlayType`),
+    // we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video
+    // element through the `src` property. This is using the built-in support
+    // of the plain video element, without using HLS.js.
+    //
+    // Note: it would be more normal to wait on the 'canplay' event below however
+    // on Safari (where you are most likely to find built-in HLS support) the
+    // video.src URL must be on the user-driven white-list before a 'canplay'
+    // event will be emitted; the last video event that can be reliably
+    // listened-for when the URL is not on the white-list is 'loadedmetadata'.
+    else if (video2.canPlayType('application/vnd.apple.mpegurl')) {
+        video2.src = videoSrc2;
+    }
+}
 /*if ("serviceWorker" in navigator) {
     // register service worker
     navigator.serviceWorker.register("service-worker.js");
